54a545b79e9bafb74a27afc861ed02024767e8a7
QusaiTA <tashtoshqusai@gmail.com>
2021-10-11 22:47:40 +0300

    solved

--- songr/src/main/java/com/songr/songr/Controller/BasicController.java
+++ songr/src/main/java/com/songr/songr/Controller/BasicController.java
@@ -1,16 +1,23 @@
 package com.songr.songr.Controller;
 
 import com.songr.songr.Model.Albums;
+import com.songr.songr.repository.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Locale;
 
 @Controller
 public class BasicController {
+
+    @Autowired
+    private AlbumRepository albumRepository;
 
     @GetMapping("/hello")
     public String greeting(@RequestParam(name="name", required = false, defaultValue = "World")String name, Model model){
@@ -30,11 +37,34 @@ public class BasicController {
             new Albums("fun album","Selena Gomez" , "https://images.indianexpress.com/2021/08/Selena-Gomez-1200.jpg",5000,20),
     };
 
+
     @GetMapping("/albums")
     public String albums(Model model){
         model.addAttribute("album",albums);
         return "albums";
     }
+
+    @GetMapping("/albums2")
+
+    public String getAlbumsAllData(Model model) {
+        model.addAttribute("albums", albumRepository.findAll());
+        return "albums";
+    }
+
+    @PostMapping("/albums2")
+    public RedirectView addNewAlbum(@RequestParam (value = "title") String title ,
+                                    @RequestParam (value = "artist") String artist,
+                                    @RequestParam (value = "imageURL") String imageURL ,
+                                    @RequestParam (value = "songCount") int songCount,
+                                    @RequestParam (value = "length")  int length)
+    {
+
+        Albums albums = new Albums(title,artist,imageURL,songCount,length);
+        albumRepository.save(albums);
+        return new RedirectView("/albums2");
+
+    }
+
 
 
 
